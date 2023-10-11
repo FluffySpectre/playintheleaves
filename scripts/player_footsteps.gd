ad697c9ef703ff1b102ace54cb0576edf2f3862e
Björn Bosse <b.bosse1991@gmail.com>
2023-10-11 13:55:21 +0200

    Tried playing the footstep sounds in a fixed sequence. Not using it, but leaving the code here

--- scripts/player_footsteps.gd
+++ scripts/player_footsteps.gd
@@ -2,15 +2,16 @@ class_name PlayerFootsteps extends Node
 
 @export var footstep_sounds_on_leafs: Array[AudioStream] = []
 @export var footstep_sounds_on_concrete: Array[AudioStream] = []
-@export var footstep_delay = 0.6
-@export var footstep_delay_running = 0.4
+@export var footstep_delay: float = 0.6
+@export var footstep_delay_running: float = 0.4
 @export var repulsor_area: RepulsorArea
 
 var footstep_timer: float = 0.0
 var is_currently_moving: bool = false
 var is_currently_jumping: bool = false
 var is_currently_landed: bool = false
-var last_footstep_sound_index = -1
+var last_footstep_sound_index: int = -1
+#var current_footstep_sound_index: int = -1
 
 @onready var parent_body: CharacterBody2D = get_parent()
 @onready var step_delay: float = footstep_delay
@@ -81,12 +82,20 @@ func stands_on_floor():
 func play_footstep_sound():
 	# is the player standing on leafs or not?
 	if stands_on_leafs() or jumped_from_leafs():
+		var sound_index = 0
+		
 		# select a random steps sound
-		var random_index = randi() % footstep_sounds_on_leafs.size()
-		while random_index == last_footstep_sound_index:
-			random_index = randi() % footstep_sounds_on_leafs.size()
-		last_footstep_sound_index = random_index
-		var sound = footstep_sounds_on_leafs[random_index]
+		sound_index = randi() % footstep_sounds_on_leafs.size()
+		while sound_index == last_footstep_sound_index:
+			sound_index = randi() % footstep_sounds_on_leafs.size()
+		last_footstep_sound_index = sound_index
+		
+		# play the footsteps sounds in order
+		#current_footstep_sound_index += 1
+		#if current_footstep_sound_index >= footstep_sounds_on_leafs.size():
+		#	current_footstep_sound_index = 0
+		#sound_index = current_footstep_sound_index
 		
 		# set and play it
+		var sound = footstep_sounds_on_leafs[sound_index]
 		SoundManager.instance.play(sound, randf_range(0.9, 1.2))
