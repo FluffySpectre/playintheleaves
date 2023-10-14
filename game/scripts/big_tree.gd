5c16078497c6aa130e2ce6b67acfea7ee5b7fed3
Björn Bosse <b.bosse1991@gmail.com>
2023-10-15 01:30:06 +0200

    Added some growl sounds, if the tree got hit

--- game/scripts/big_tree.gd
+++ game/scripts/big_tree.gd
@@ -1,8 +1,11 @@
 class_name BigTree extends Node2D
 
 @export var fade_speed: float = 1.0
+@export var growl_sounds: Array[AudioStream]
 
 var anger_level: int = 0
+var last_anger_level: int = 0
+var last_played_growl_index: int = -1
 
 @onready var tree_sprite_normal: Sprite2D = $NormalSprite2D
 @onready var tree_sprite_middle: Sprite2D = $MiddleSprite2D
@@ -17,3 +20,16 @@ func _process(delta):
 	elif anger_level == 0:
 		tree_sprite_normal.modulate.a = lerp(tree_sprite_normal.modulate.a, 1.0, delta * fade_speed)
 		tree_sprite_middle.modulate.a = lerp(tree_sprite_middle.modulate.a, 1.0, delta * fade_speed)
+
+	if anger_level != last_anger_level:
+		last_anger_level = anger_level
+		play_growl()
+		
+func play_growl():
+	# select a random steps sound
+	var sound_index = randi() % growl_sounds.size()
+	while sound_index == last_played_growl_index:
+		sound_index = randi() % growl_sounds.size()
+	last_played_growl_index = sound_index
+	SoundManager.instance.play(growl_sounds[sound_index])
+	
